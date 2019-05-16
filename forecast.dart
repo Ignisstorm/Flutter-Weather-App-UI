d0f2a6f36a23e6651de6aacc88bc2bf31081e24c
Erika Loots <43708340+Ignisstorm@users.noreply.github.com>
2019-05-16 10:08:08 +0200

    Add files via upload

---
+++ forecast.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:weather_app/forecast/background/background_with_rings.dart';
+import 'package:weather_app/forecast/background/radial_list.dart';
+import 'package:weather_app/forecast/background/rain.dart';
+
+class Forecast extends StatelessWidget {
+
+  RadialListViewModel radialList;
+  SlidingRadialListController slidingListController;
+
+  Forecast({
+    @required this.radialList,
+    @required this.slidingListController,
+});
+
+  Widget _temperatureText(){
+    return Align(
+      alignment: Alignment.centerLeft ,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 150.0, left: 10.0),
+        child: new Text(
+          '68°',
+          style: new TextStyle(
+            color: Colors.white,
+            fontSize: 80.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        new BackgroundWithRings(),
+
+
+
+        _temperatureText(),
+
+        new SlidingRadialList(
+        radialList: radialList,
+          controller:slidingListController,
+        ),
+        new Rain(),
+      ],
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:weather_app/forecast/background/app_bar.dart';
+import 'package:weather_app/forecast/background/background_with_rings.dart';
+import 'package:weather_app/forecast/background/forecast.dart';
+import 'package:weather_app/forecast/background/forecast_list.dart';
+import 'package:weather_app/forecast/background/radial_list.dart';
+import 'package:weather_app/forecast/background/week_drawer.dart';
+import 'package:weather_app/generic_widgets/sliding_drawer.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Weather',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+
+  OpenableController openableController;
+  SlidingRadialListController slidingListController;
+  String selectedDay = 'Monday, August 26';
+
+  @override
+  void initState() {
+    super.initState();
+
+    openableController = new OpenableController(
+      vsync: this,
+      openDuration: const Duration(milliseconds: 250),
+    )
+      ..addListener(() => setState(() {}));
+
+    slidingListController = new SlidingRadialListController(
+      itemCount: forecastRadialList.items.length,
+      vsync: this,
+    )
+      ..open();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          new Forecast(
+            radialList: forecastRadialList,
+            slidingListController: slidingListController,
+          ),
+
+          new Positioned(
+            top: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: new ForecastAppBar(
+              onDrawerArrowTap: openableController.open,
+              selectedDay: selectedDay,
+            ),
+          ),
+
+          new SlidingDrawer(
+            openableController: openableController,
+            drawer: new WeekDrawer(
+              onDaySelected: (String title) {
+                setState(() {
+                  selectedDay = title.replaceAll('\n', ', ');
+                });
+
+                slidingListController
+                    .close()
+                    .then((_) => slidingListController.open());
+
+                openableController.close();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
